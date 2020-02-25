39cb774a49990b2efc880f5db93ea74ffac4c506
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-02-25 18:05:48 -0300

    Add function deleteToDo() and update function renderToDos()

--- js/main.js
+++ js/main.js
@@ -2,13 +2,9 @@ let listElement = document.querySelector('div#app ul')
 let inputElement = document.querySelector('div#app input')
 let buttonElement = document.querySelector('div#app button')
 
-let toDos = [
-    'Teste 1',
-    'Teste 2',
-    'Teste 3'
-]
+let toDos = []
 
-// Renderizar
+// Renderizar toDos
 function renderToDos() {
     listElement.innerHTML = ''  //limpa a lista de elementos anterior sempre que houver uma nova renderização (para não colocar novamente os elem que já estão)
 
@@ -16,15 +12,28 @@ function renderToDos() {
         let toDoElement = document.createElement('li')
         let toDoText = document.createTextNode(todo)  //propria variavel pode ser adicionada (contém o texto de cada to do)
 
+        let linkElement = document.createElement('a')
+        
+        linkElement.setAttribute('href', '#')  //adiciona href ao link criado
+
+        let pos = toDos.indexOf(todo)  //procura dentro do array o indíce do elemento atual que está passando pela função
+        linkElement.setAttribute('onclick', `deleteToDo(${pos})`)
+
+        let linkText = document.createTextNode('Excluir')  //texto dentro do elemento
+
+        linkElement.appendChild(linkText)
+
         toDoElement.appendChild(toDoText)
+        toDoElement.appendChild(linkElement)
+
         listElement.appendChild(toDoElement)
         }
     }
 
     renderToDos()
 
-    // Adicionar
-    function addToDos() {
+    // Adicionar toDos
+    function addToDo() {
         let toDoText = inputElement.value  //valor do input
 
         toDos.push(toDoText)  //adiciona o valor do input ao array
@@ -34,4 +43,9 @@ function renderToDos() {
         renderToDos()  //renderiza todos os todos novamentes com o novo que foi adicionado ao final do array
     }
 
-    buttonElement.onclick = addToDos  //executa a adição somente no clique do botão
+    buttonElement.onclick = addToDo  //executa a adição somente no clique do botão
+
+    function deleteToDo(position) {
+        toDos.splice(position, 1)  //remover próximo item a partir do item atual que usuário clicar (position)
+        renderToDos()              //renderizar novamente
+    }
